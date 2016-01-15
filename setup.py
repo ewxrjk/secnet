749aa78cf12496cca4b9bf3eafe045809fc0da10
SunDwarf <eyesismine@gmail.com>
2016-01-15 19:28:27 +0000

    Add setup.py and move base91.py to a package
    Signed-off-by: SunDwarf <eyesismine@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='base91',
+    version='1.0.1',
+    packages=['base91'],
+    url='https://github.com/SunDwarf/base91-python',
+    license='New BSD',
+    author='',
+    author_email='',
+    description='Base91 encoding in Python'
+)
