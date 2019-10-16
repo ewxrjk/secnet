81a29dce18b7298c88635a2bfbee52fb16ded3b9
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-10-16 21:16:38 +0100

    make-secnet-sites: Put parens around print() statements
    This is part of the transition to python3.
    
    In actual fact these are all error messages and should go to stderr
    but I'm ot fixing that right now.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- ipaddrset-test.py
+++ ipaddrset-test.py
@@ -29,6 +29,8 @@
 # The corresponding test vector file ise ipaddrset-test.expected.  I
 # don't believe it is a creative work that attracts copyright.  -iwj.
 
+from __future__ import print_function
+
 import ipaddr
 from ipaddr import IPNetwork, IPAddress
 
@@ -38,33 +40,33 @@ from ipaddrset import IPAddressSet
 v4a=IPAddress('172.18.45.6')
 
 s=IPAddressSet()
-print 's =', s
+print('s =', s)
 s.append([IPNetwork('172.18.45.0/24')])
 s.append([IPNetwork('2001:23:24::/48')])
-print s
+print(s)
 
 t=IPAddressSet(map(IPNetwork,['172.31.80.8/32','172.18.45.192/28']))
-print 't =', t
-print t <= s
-print t == s
+print('t =', t)
+print(t <= s)
+print(t == s)
 
 for n1s in ['172.18.44.0/23','172.18.45.6/32','172.18.45.0/24']:
     n1=IPNetwork(n1s)
-    print n1
-    print s.contains(n1)
-    print t.contains(n1)
+    print(n1)
+    print(s.contains(n1))
+    print(t.contains(n1))
 
 n=s.networks()[0]
 
 a=ipaddrset.complete_set()
-print 'a =', a
-print a >= s
-print a >= t
+print('a =', a)
+print(a >= s)
+print(a >= t)
 
-print '^'
-print s.intersection(t)
-print t.intersection(s)
+print('^')
+print(s.intersection(t))
+print(t.intersection(s))
 
-print 'u'
-print s.union(t)
-print t.union(s)
+print('u')
+print(s.union(t))
+print(t.union(s))
