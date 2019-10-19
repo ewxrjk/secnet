5042178018d32875ecb0b6c8d1a72f14fe95b058
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-10-19 23:19:58 +0100

    stest: Use proper builddir subdir as default tmp
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- stest/common.tcl
+++ stest/common.tcl
@@ -164,18 +164,19 @@ proc sendpkt {} {
 }
 
 if {![catch {
-    set tmp $env(AUTOPKGTEST_ARTIACTS)
-}]} {} elseif {![catch {
-    set tmp $env(AUTOPKGTEST_TMP)
-}]} {} elseif {[regsub {^stest/t-} $argv0 {stest/d-} tmp]} {
-    file mkdir $tmp
-}
-if {![catch {
     set builddir $env(STEST_BUILDDIR)
 }]} {} else {
     set builddir .
 }
 
+if {![catch {
+    set tmp $env(AUTOPKGTEST_ARTIACTS)
+}]} {} elseif {![catch {
+    set tmp $env(AUTOPKGTEST_TMP)
+}]} {} elseif {[regsub {^stest/t-} $argv0 {stest/d-} tmp]} {
+    set tmp $builddir/$tmp
+    file mkdir $tmp
+}
 set socktmp $tmp/s
 exec mkdir -p -m700 $socktmp
 regsub {^(?!/)} $socktmp {./} socktmp ;# dgram-socket wants ./ or /
