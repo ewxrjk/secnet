e8a5679784c19508f1f93f3e51f315cf3977b187
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-10-20 12:08:48 +0100

    mtest: Break out mss-run-userv
    No functional change.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- mtest/common.tcl
+++ mtest/common.tcl
@@ -1,5 +1,15 @@
 source test-common.tcl
 
+proc mss-program {} {
+    set l ./make-secnet-sites
+}
+
+proc run-mss-userv {user group args} {
+    eval [list exec env USERV_USER=$user USERV_GROUP=$group] \
+	 [mss-program] \
+	 $args
+}
+
 file mkdir $tmp/groupfiles
 
 prefix_some_path PYTHONPATH .
