e68a917bdc09e3d332edbaa171347ddfe1902d59
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-10-20 12:02:21 +0100

    test-common: Handle mtest correctly too
    The default value for tmp nees to be right for mtest/ too.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- test-common.tcl
+++ test-common.tcl
@@ -17,7 +17,7 @@ if {![catch {
     set tmp $env(AUTOPKGTEST_ARTIACTS)
 }]} {} elseif {![catch {
     set tmp $env(AUTOPKGTEST_TMP)
-}]} {} elseif {[regsub {^stest/t-} $argv0 {stest/d-} tmp]} {
+}]} {} elseif {[regsub {^([sm]test)/t-} $argv0 {\1/d-} tmp]} {
     set tmp $builddir/$tmp
     file mkdir $tmp
 }
