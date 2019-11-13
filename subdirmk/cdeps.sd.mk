6600b10418c5c38d43bf6c732724123ed34779b6
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-11-13 22:04:32 +0000

    Introduce CDEPS_CFLAGS so without cdeps.sd.mk you don't get .*.d
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- subdirmk/cdeps.sd.mk
+++ subdirmk/cdeps.sd.mk
@@ -3,6 +3,8 @@
 #  Copyright 2019 Ian Jackson
 # SPDX-License-Identifier: LGPL-2.0-or-later
 
+CDEPS_CFLAGS ?= -MD -MF $(*D)/.$(*F).d
+
 &DEPFILES += $(foreach b,$(patsubst %.o,%,$(&OBJECTS)), \
 		$(dir $b).$(notdir $b).d)
 -include $(&DEPFILES)
