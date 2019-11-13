25a516a59e7811b8626b5fe40b49c5ef942db697
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-11-13 22:31:43 +0000

    Transpose directories
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- subdirmk/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# subdirmk, autogen.sh (conventional autoconf invocation script)
-#  Copyright 2019 Ian Jackson
-# SPDX-License-Identifier: LGPL-2.0-or-later
-set -e
-cd ${0%/*}
-autoconf
