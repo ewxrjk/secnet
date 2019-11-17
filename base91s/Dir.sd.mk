d31e77241cb84539fa203ce41d739ed2841bfbff
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-11-17 01:49:18 +0000

    base91: Patch the C version for our charset change
    The base91.c.patch file was made by editing base91.c by hand and
    running git diff.  The change to enctab is the obvious one.
    The change to dectab was simply done by hand based on inspection.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- base91s/Dir.sd.mk
+++ base91s/Dir.sd.mk
@@ -5,9 +5,11 @@
 &CFILES += & base91.c base91.h cli.c
 &CLEAN += $(&CFILES)
 
+&base91.c: &^base91.c.patch
 $(&CFILES): &/%: &~/base91-c/% &/Dir.mk
 	perl -pe <$< >$@.tmp \
  'next if m{^\#include}; s/basE91/base91s/g; s/base91\b/base91s/g'
+	patch <$(or $(filter %.patch,$^),/dev/null) $@.tmp
 	mv -f $@.tmp $@
 
 $(&OBJECTS): &base91.h
