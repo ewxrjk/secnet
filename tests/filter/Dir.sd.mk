bf6d5c55416bab7bd8a1dfed010cd9c240aaaddf
Ian Jackson <ijackson@chiark.greenend.org.uk>
2019-12-28 12:58:03 +0000

    test/filter; Cause some warnings, to check they appear
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- tests/filter/Dir.sd.mk
+++ tests/filter/Dir.sd.mk
@@ -1,5 +1,15 @@
 # test cases for generate script
 
+# some blank/comment lines to make "saw WARN" come out with 1-
+# and 2-digit line numbers
+
+WARN += 3
+&WARN += 3
+# $WARN
+# $(WARN)
+# $(&WARN)
+# &$WARN
+
 &${ some-macro, 42, $x, { &$- $(foreach something) } }
 
 # doctests:
